68d8ce4a0b6b93dfa8a9ff581d8b82fd9a6f6146
commandercool <erohin_a_v@mail.ru>
2021-04-12 11:49:34 +0300

    Initial commit

---
+++ src/SSLCheck.java
@@ -0,0 +1,26 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SSLCheck {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar SSLPing.jar <host> <port>");
+            System.exit(1);
+        }
+
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        System.out.println("About to connect to '" + hostname + "' on port " + port);
+
+        SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+
+        try (SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(hostname, port)) {
+            sslsocket.startHandshake();
+            System.out.println("Successfully connected");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
